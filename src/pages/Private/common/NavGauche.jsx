32f3ca482cad1bcd3cda1d26aecb56754d3b4e4e
gulliano <ggulliano@msn.com>
2023-09-07 13:48:31 -0400

    no match

--- src/pages/Private/common/NavGauche.jsx
+++ src/pages/Private/common/NavGauche.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {   Link } from "react-router-dom";
 
 const NavGauche = () => {
   return (
@@ -10,32 +11,32 @@ const NavGauche = () => {
 
         <ul className="space-y-2 tracking-wide mt-8">
             <li>
-                <a href="#" aria-label="dashboard" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
+                <Link to="/" aria-label="dashboard" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
                     <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
                         <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
                         <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
                         <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
                     </svg>
                     <span className="-mr-1 font-medium">Dashboard</span>
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                <Link to="/category" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path className="fill-current text-gray-300 group-hover:text-cyan-300" fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
                         <path className="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
                     </svg>
                     <span className="group-hover:text-gray-700">Categories</span>
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                <Link to="/question" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path className="fill-current text-gray-600 group-hover:text-cyan-600" fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd" />
                         <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
                     </svg>
-                    <span className="group-hover:text-gray-700">Reports</span>
-                </a>
+                    <span className="group-hover:text-gray-700">Questions</span>
+                </Link>
             </li>
             <li>
                 <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
@@ -58,8 +59,15 @@ const NavGauche = () => {
         </ul>
     </div>
 
+       
+
     <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+
+
+
      
+  
+
     </div>
 </aside>
   )
--- src/pages/Private/index.jsx
+++ src/pages/Private/index.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 import { logout } from '../../commonjs/auth'
 import NavGauche from './common/NavGauche'
 import NavBar from './common/NavBar'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Layout from './common/Layout';
+import Category from './Category';
+import Home from './Home';
+import NoMatch from '../NoMatch';
+import Question from './Question';
+
 
 const Private = () => {
   return (
    <>
   
-<NavGauche />
+ <NavGauche /> 
 <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
  <NavBar />
 
     <div className="px-6 pt-6 2xl:container">
    
     {/* Container */}
+
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home/>} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/question' element={<Question />} />
+        <Route path='*' element={<NoMatch />} />
+
+      </Route>
+    </Routes>
+
+
+
 
 
     </div>
@@ -24,3 +44,6 @@ const Private = () => {
 }
 
 export default Private
+
+
+
